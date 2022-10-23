99a13b52a176b9dfab56d92d1aa418927d4d1972
dominikmuller <d.mueller@emplify-software.de>
2022-10-23 11:11:42 +0200

    Automatically publish files after statamic installation

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace Doefom\Jobboard;
 
+use Illuminate\Support\Facades\Artisan;
 use Statamic\Facades\CP\Nav;
 use Statamic\Providers\AddonServiceProvider;
+use Statamic\Statamic;
 
 class ServiceProvider extends AddonServiceProvider
 {
@@ -14,12 +16,16 @@ class ServiceProvider extends AddonServiceProvider
 
     public function bootAddon()
     {
+        parent::bootAddon();
 
         $this->publishes([
             __DIR__ . '/../content/collections' => base_path('content/collections'),
             __DIR__ . '/../resources/blueprints' => resource_path('blueprints'),
-        ]);
+        ], 'doefom/jobboard');
 
+        Statamic::afterInstalled(function ($command) {
+            $command->call('vendor:publish', ['--tag' => 'doefom/jobboard']);
+        });
     }
 
 }
