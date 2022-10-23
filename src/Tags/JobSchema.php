4f8d95a072e70abc2c78bee0fc4ea401c1e94a95
dominikmuller <d.mueller@emplify-software.de>
2022-10-23 14:01:05 +0200

    Remove instructinons from title field in jobs_reusable_content.yaml. Add JobSchema Tag. Link values of reusable contents correctly.

---
+++ src/Tags/JobSchema.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Doefom\Jobboard\Tags;
+
+use Statamic\Tags\Tags;
+
+class JobSchema extends Tags
+{
+
+    /**
+     * The {{ job_schema }} tag.
+     *
+     * @return string|array
+     */
+    public function index(): array|string
+    {
+
+    }
+
+}
--- src/Tags/Jobs.php
+++ src/Tags/Jobs.php
@@ -8,8 +8,6 @@ use Statamic\Tags\Tags;
 class Jobs extends Tags
 {
 
-    protected static $handle = 'jobs';
-
     /**
      * The {{ jobs }} tag.
      *
@@ -17,7 +15,30 @@ class Jobs extends Tags
      */
     public function index(): array|string
     {
-        return Entry::query()->where('collection', 'jobs')->get()->toArray();
+        $jobs = Entry::query()
+            ->where('collection', 'jobs')
+            ->get();
+
+        // Output linked
+        $jobs = $jobs->map(function ($job) {
+
+            if ($job->link_reusable_content_responsibilities) {
+                $job->responsibilites = $job->reusable_content_responsibilities->content;
+            }
+
+            if ($job->link_reusable_content_qualifications) {
+                $job->qualifications = $job->reusable_content_qualifications->content;
+            }
+
+            if ($job->link_reusable_content_incentives) {
+                $job->incentives = $job->reusable_content_incentives->content;
+            }
+
+            return $job;
+
+        });
+
+        return $jobs->toArray();
     }
 
 }
